a158d09779db19f3f3bf135e367284daf4a659bc
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2025-11-27 17:15:13 +0100

    feat: agregar cliente y server supabase

---
+++ lib/supabase/server.ts
@@ -0,0 +1,26 @@
+import { cookies } from "next/headers"
+import { createServerClient, type CookieOptions } from "@supabase/ssr"
+
+export const createSupabaseServerClient = async () => {
+  const cookieStore = await cookies()
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll().map((cookie) => ({
+            name: cookie.name,
+            value: cookie.value,
+          }))
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            cookieStore.set(name, value, options as CookieOptions)
+          })
+        },
+      },
+    },
+  )
+}
